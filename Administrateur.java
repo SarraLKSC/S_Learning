1f4a8fe4829b728be9c75a562539e8382ef48aef
SarraLKSC <44327683+SarraLKSC@users.noreply.github.com>
2020-02-07 08:59:05 +0100

    2nd upload
    Adding the 2nd set of Java classes to the repo

---
+++ Administrateur.java
@@ -0,0 +1,11 @@
+package sample;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Administrateur extends User {
+    Administrateur(String nom, String prenom,String mail, String motDpass, String langue, String image,String heure){
+        super(nom,prenom,mail,motDpass,langue,image,heure);
+
+    }
+}
---
+++ Blog.java
@@ -0,0 +1,6 @@
+package sample;
+
+public class Blog {
+    void addBlog(){}
+    void deleteBlog(){}
+}
